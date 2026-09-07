9276f9b9a6cbd02083b85bd7c0aed11868fee17e
Anant Singh Shishodia <anantthakur2005@gmail.com>
2026-09-08 00:47:30 +0530

    Configure redux toolkit in projects

--- 12Megablog/src/appwrite/auth.js
+++ 12Megablog/src/appwrite/auth.js
@@ -3,61 +3,59 @@ import config from "../config/config.js";
 import { Client, Account, ID } from "appwrite";
 
 export class AuthService {
-    client= new Client();
-    account;
+  client = new Client();
+  account;
 
-    constructor(){
-        this.client
-            .setEndpoint(config.appwriteUrl)
-            .setProject(config.appwriteProjectId);
-                this.account=new Account(this.client);
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteUrl)
+      .setProject(config.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
 
-        }
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name,
+      );
+      if (userAccount) {
+        //call another method
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
 
-        async createAccount({email,password,name}){
-            try{
-             const userAccount=await this.account.create(ID.unique(),email, password,name);
-             if(userAccount){
-              //call another method
-            return this.login({email,password});
-             }
-             else{
-                return userAccount;
-             }
-            }
-            catch(error){
-              throw error;
-            }
-        }
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
 
-        async login({email,password}){
-            try{
-           return await this.account.createEmailSession(email,password);
-            }
-            catch(error){
-             throw error;
-            }
-        }
-
-        async getCurrentUser(){
-            try{
-               return await this.account.get()
-            }
-            catch(error){
-                throw error;
-            }
-        }
-
-        async logout(){
-            try{
-              return await this.account.deleteSessions();
-            }
-            catch(error){
-                throw error;
-            }
-        }
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      return null;
+    }
+  }
+  async logout() {
+    try {
+      return await this.account.deleteSessions();
+    } catch (error) {
+      throw error;
+    }
+  }
 }
 
-const authService= new AuthService();
+const authService = new AuthService();
 
 export default authService;
--- 12Megablog/src/config/config.js
+++ 12Megablog/src/config/config.js
@@ -6,7 +6,7 @@ appwriteProjectId:String (import.meta.env.VITE_APPWRITE_PROJECT_ID),
 
 appwriteDatabaseId:String (import.meta.env.VITE_APPWRITE_DATABASE_ID),
 
-appwriteCollectionId: StringString(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
 
 appwriteBucketId:String (import.meta.env.VITE_APPWRITE_BUCKET_ID)
 
